15302b01d7e4ce7e926ee7ae424918e47493ac37
Wellington Silva <wellington9850@live.com>
2021-03-01 17:11:40 -0300

    Repo init

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => (
+    <div>
+        <div>
+            <h1>Ganhe sua liberdade para ir e vir</h1>
+            <ul>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis dolor nobis neque labore ut nostrum, quo, unde veritatis illum tempore debitis excepturi, earum mollitia quidem.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis dolor nobis neque labore ut nostrum, quo, unde veritatis illum tempore debitis excepturi, earum mollitia quidem.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis dolor nobis neque labore ut nostrum, quo, unde veritatis illum tempore debitis excepturi, earum mollitia quidem.</li>
+            </ul>
+            <button>Matricule-se agora</button>
+        </div>
+    </div>
+)
+
+export default Hero;
---
+++ src/stories/Hero.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Hero from '../components/Hero';
+
+export default {
+    title: 'Hero',
+    component: Hero
+};
+
+export const usage = () => <Hero />;
